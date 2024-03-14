74d9e3d898fe7e351f8adba8db405ee33147586c
vzanbr <gabriecvr123@gmail.com>
2024-03-14 13:23:08 -0300

    Feat: Criado em form metodo para salvar

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -10,18 +10,16 @@ import { Cliente } from '../cliente';
 export class ClientesFormComponent implements OnInit {
 
   cliente: Cliente;
-  nome: string = 'Digite seu nome';
 
   constructor() { 
     this.cliente = new Cliente();
-    this.cliente.nome = 'gabriel';
   }
 
   ngOnInit(): void {
   }
 
-  clicar() {
-    console.log('Cliquei!');
+  onSubmit() {
+    console.log(this.cliente);
   }
     
 
