493b754d2241fc34534430c4249ce7de47110448
vzanbr <gabriecvr123@gmail.com>
2024-03-15 16:04:35 -0300

    Feat : new functions

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/clientes/cliente';
+import { ClientesService } from 'src/app/clientes.service';
+import { ServicoPrestado } from '../servicoPrestado';
+import { ServicoPrestadoService } from 'src/app/servico-prestado.service';
 
 @Component({
   selector: 'app-servico-prestado-form',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicoPrestadoFormComponent implements OnInit {
 
-  constructor() { }
+  clientes: Cliente[] = []
+  servico : ServicoPrestado;
+
+  constructor(
+    private clienteService: ClientesService,
+    private service: ServicoPrestadoService
+  ) {
+    this.servico = new ServicoPrestado();
+   }
 
   ngOnInit(): void {
+    this.clienteService
+      .getClientes()
+      .subscribe(reponse => this.clientes = reponse);
   }
 
+  onSubmit() {
+    this.service.
+    salvar(this.servico)
+    .subscribe(response => {
+      console.log(response)
+    })
+  }
 }
