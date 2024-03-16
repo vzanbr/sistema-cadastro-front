042ff160e7f8c819dd611e8cd675cd3aec5a3577
vzanbr <gabriecvr123@gmail.com>
2024-03-16 15:28:07 -0300

    Feature : novas rotas com alteração de path e fazendo herança

--- src/app/servico-prestado/servico-prestado-routing.module.ts
+++ src/app/servico-prestado/servico-prestado-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ServicoPrestadoFormComponent } from './servico-prestado-form/servico-prestado-form.component';
 import { ServicoPrestadoListaComponent } from './servico-prestado-lista/servico-prestado-lista.component';
+import { LayoutComponent } from '../layout/layout.component';
 
 
 const routes: Routes = [
-  {
-    path: 'servico-prestado-form', component: ServicoPrestadoFormComponent},
-    {path: 'servico-prestado-lista', component: ServicoPrestadoListaComponent
-  }
+  { path: 'servicos', component: LayoutComponent, children: [
+    {path: 'form', component: ServicoPrestadoFormComponent},
+      {path: 'lista', component: ServicoPrestadoListaComponent},
+      { path: '', redirectTo: '/servicos/lista', pathMatch: 'full'  }
+  ]}
 ];
 
 @NgModule({
