ff598a750c6e21800b375f894dd3a325fb85d15e
vzanbr <gabriecvr123@gmail.com>
2024-03-16 15:32:39 -0300

    feat: Inserindo funcionalidades de metodos e navegação /home

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  username: string;
+  password: string;
+  loginError: boolean;
+  cadastrando: boolean;
 
-  ngOnInit(): void {
-  }
+  constructor(
+    private router : Router
+  ) {}
 
+    onSubmit() {
+      this.router.navigate(['/home'])
+   }
+
+   preparaCadastrar(event) {
+    event.preventDefault();
+    this.cadastrando = true;
+   }   
+   
+   cancelaCadastro() {
+    this.cadastrando = false;
+   }
 }
