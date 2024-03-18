4f1a73639c0dabc108cbc36d28e57e907e3bd527
vzanbr <gabriecvr123@gmail.com>
2024-03-18 16:33:46 -0300

    Feat: Implementando Auth. para segurança

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,22 +1,74 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Usuario } from './login/usuario';
 import { Observable } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 
+import { JwtHelperService } from '@auth0/angular-jwt'
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  loginUrlCriar: string = environment.loginUrlCriar
+  apiUrl: string = environment.apiURL;
+  loginUrlCriar: string = environment.loginUrlCriar;
+  tokenURL: string = environment.obterToken;
+  clientID: string = environment.clientId;
+  clientSecret: string = environment.clientSecret;
+  jwtHelper : JwtHelperService = new JwtHelperService();
 
   constructor(
     private http: HttpClient
   ) { }
 
+  obterToken(){
+    const tokenString = localStorage.getItem('access_token')
+    if(tokenString){
+      const token = JSON.parse(tokenString).access_token
+      return token;
+    }
+    return null;
+  }
+
+  encerrarSessao(){
+    localStorage.removeItem('access_token')
+  }
+
+  getUsuarioAutenticado(){
+    const token = this.obterToken();
+    if(token){
+      const usuario = this.jwtHelper.decodeToken(token).user_name
+      return usuario;
+    }
+    return null;
+  }
+
+  isAuthenticated() : Boolean {
+    const token = this.obterToken();
+    if(token){
+      const expired = this.jwtHelper.isTokenExpired(token)
+      return !expired;
+    }
+    return false;
+  }
+
   salvar(usuario : Usuario) : Observable<any> {
     return this.http.post(this.loginUrlCriar, usuario);
+  }  
+  
+  tentarLogar( username: string, password: string ) : Observable<any> {
+    const params = new HttpParams()
+                        .set('username', username)
+                        .set('password', password)
+                        .set('grant_type', 'password')
+
+    const headers = {
+      'Authorization': 'Basic ' + btoa(`${this.clientID}:${this.clientSecret}`),
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    return this.http.post(this.tokenURL, params.toString() , { headers });
   }
 }
