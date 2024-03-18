f34596c88660a174b7658d874583f4c61174af4b
vzanbr <gabriecvr123@gmail.com>
2024-03-18 16:37:07 -0300

    Feat: Novos Urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   apiURL: 'http://localhost:1000',
-  apiServPresURL: 'http://localhost:1000/servico',
+  apiClienteURL: 'http://localhost:1000/cliente/salvar',
+  apiClURL: 'http://localhost:1000/cliente',
+  apiSlURL: 'http://localhost:1000/servicos',
   loginUrlCriar: 'http://localhost:1000/salvar/usuario',
+  clientId: 'my-angular-app',
+  clientSecret: '@321',
+  obterToken: 'http://localhost:1000/oauth/token'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
